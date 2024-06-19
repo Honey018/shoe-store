7f6eef98e595e1e4c77a586ac09d1782157b041b
Honey Rajput <144479482+Honey018@users.noreply.github.com>
2024-06-19 17:37:04 +0530

    Shoe-Store

---
+++ lib/global_variable.dart
@@ -0,0 +1,34 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.52,
+    'imageUrl': 'assets/images/shoe1.png',
+    'company': 'Nike',
+    'sizes': [9, 10, 11, 12],
+  },
+  {
+    'id': '1',
+    'title': 'Addidas Shoes',
+    'price': 20.12,
+    'imageUrl': 'assets/images/shoe5.png',
+    'company': 'Addidas',
+    'sizes': [9, 10, 12],
+  },
+  {
+    'id': '2',
+    'title': 'Puma Men\'s Shoes',
+    'price': 28.95,
+    'imageUrl': 'assets/images/shoe7.png',
+    'company': 'Puma',
+    'sizes': [8, 9, 10],
+  },
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'imageUrl': 'assets/images/shoe4.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+  },
+];
